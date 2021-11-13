e3c7d34edebe61528e6c7532b98f9d2bd00a6715
Diana Jiang <jiang99diana@gmail.com>
2021-11-13 12:52:10 -0500

    commit tree 3513e29e5f5f5b5cbf29e8d0e5a03244e8db3737

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(driverNames, possibleName) {
+    return driverNames.filter(name =>
+        name.toLowerCase() === possibleName.toLowerCase());
+};
+
+function fuzzyMatch(driverNames, possibleName) {
+    return driverNames.filter(name => name[0] === possibleName[0]);
+};
+
+function matchName(driver, input) {
+    return driver.filter(property => property.name === input);
+} 
